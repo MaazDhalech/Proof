ad81ac2104ba8de12574a02b2ff8ebaa0b1be512
Babiker Babiker <bbabiker09@gmail.com>
2025-06-21 17:28:44 -0700

    added session persistance to limit amount of times user has to login

--- Proof/services/supabase.ts
+++ Proof/services/supabase.ts
@@ -1,5 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 import Constants from 'expo-constants';
+import * as SecureStore from 'expo-secure-store';
 
 const {
   SUPABASE_URL,
@@ -10,4 +11,17 @@ if (!SUPABASE_URL || !ANON_PUBLIC_KEY) {
   throw new Error('Missing Supabase environment variables.');
 }
 
-export const supabase = createClient(SUPABASE_URL, ANON_PUBLIC_KEY);
+const ExpoSecureStoreAdapter = {
+  getItem: (key: string) => SecureStore.getItemAsync(key),
+  setItem: (key: string, value: string) => SecureStore.setItemAsync(key, value),
+  removeItem: (key: string) => SecureStore.deleteItemAsync(key),
+};
+
+export const supabase = createClient(SUPABASE_URL, ANON_PUBLIC_KEY, {
+  auth: {
+    storage: ExpoSecureStoreAdapter,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
+});
